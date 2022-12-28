c46c0a081e345993e842211ec6296c2cdb51abe4
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-12-28 16:38:39 +0000

    css changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
+import useMultiStepForm from "./useMultiStepForm"
 
 
 function App() {
-  
+  const {
+    
+  } = useMultiStepForm([])
 
   return (
-<div style={{
-  position:"relative",
-  background:"white",
-  border: "1px solid black",
-  padding: "2 rem",
-  margin: "1 rem",
-  borderRadius: ".5rem",
-  fontFamily: "Arial",
-}}>
+    <div
+    style={{
+      position: "relative",
+      background: "white",
+      border: "1px solid black",
+      padding: "2rem",
+      margin: "1rem",
+      borderRadius: ".5rem",
+      fontFamily: "Arial",
+      maxWidth: "max-content",
+    }}
+  >
   <form>
-<div style={{position: "absolute",top: ".5rem", right:".5rem"}}></div>
+<div style={{ position: "absolute", top: ".5rem", right: ".5rem" }}></div>
   </form>
 </div>
   )
