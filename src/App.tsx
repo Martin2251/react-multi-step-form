b80c2dcba3ef74b346fa6ce9fc82aebd91d7eafb
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-12-28 09:00:36 +0000

    basic style

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,19 @@ function App() {
   
 
   return (
-<h1>hi</h1>
+<div style={{
+  position:"relative",
+  background:"white",
+  border: "1px solid black",
+  padding: "2 rem",
+  margin: "1 rem",
+  borderRadius: ".5rem",
+  fontFamily: "Arial",
+}}>
+  <form>
+<div style={{position: "absolute",top: ".5rem", right:".5rem"}}></div>
+  </form>
+</div>
   )
 }
 
